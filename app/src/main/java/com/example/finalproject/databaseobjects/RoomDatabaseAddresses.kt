8641ee35ac75aca27d00c022a4a469ec146402c1
Matthew Palkowski <matthewpalkowski@gmail.com>
2021-05-09 09:19:07 -0400

    Minor changes to room database classes to address build errors.

--- app/src/main/java/com/example/finalproject/databaseobjects/RoomDatabaseAddresses.kt
+++
@@ -1,8 +0,0 @@
-package com.example.finalproject.databaseobjects
-
-import androidx.room.Database
-
-@Database(entities = [AddressEntity::class], version = 1)
-abstract class RoomDatabaseAddresses {
-    abstract fun contactDAO() : AddressDAO
-}
